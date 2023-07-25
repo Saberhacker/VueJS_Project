1c25ca555a66dc760ea65d8ff990cac320ced4b1
saber-seerat <saber.seerat66@gmail.com>
2023-07-25 09:27:07 +0430

    fourth change

--- src/App.vue
+++ src/App.vue
@@ -1,133 +1,103 @@
 <template>
-    <!-- <h1>{{greeting}} {{name}}</h1>
-    <h2 v-text="lastname"></h2>
-    <h2 v-html="channel"></h2>
-    <h2 v-bind:id="headindId">Heading</h2>
-    <button v-bind:disabled="isDisabled">Bind</button>
-    <h2 class="underlined">Underlined text</h2>
-    <h2 class="underlined" v-bind:class="status">status</h2>
-    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
-    <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">SoldOut Movie</h2>
-    <h2 v-bind:class="['new','promoted']">Newly promoted Movie</h2>
-    <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">Array conditional Movie</h2> -->
-    <!-- <h2 v-bind:class="{
-      promoted: isPromoted,
-      new: !isSoldOut,
-      'sold-out': isSoldOut
-    }">Object conditional Movie</h2>
-    <h2 v-bind:style="{
-      color: highlightColor,
-      fontSize: headerSize + 'px',
-      padding: '20px'                                                                        
-    }">Inline Style</h2>
-    <h2 v-bind:style="headerStyleObject">Style Object</h2> -->
-
-    <!-- <div v-bind:style="[baseStyleObject,successStyleObject]">Success Style</div> -->
-    <!-- <div v-bind:style="[baseStyleObject,dangerStyleObject]">Danger Style</div> -->
-    <!-- <h2 v-if="num == 0">The number is zero</h2>
-    <h2 v-else-if="num < 0">The number is negative</h2>
-    <h2 v-else-if="num > 0">The number is positive</h2>
-    <h2 v-else>Not a number</h2>
-    <template v-if="display">
-      <h2>Saber.Seerat</h2>
-      <h2>Lajaward</h2>
-      <h2>Vue JS</h2> -->
-    <!-- </template> -->
-    <!-- <h2 v-for="(name,index) in names" :key='name'>{{index}} {{name}}</h2>
-    <h2 v-for="name in fullname" :key="name.first">{{name.first}} {{name.last}}</h2>
-    <h3 v-for="(value,key,index) in myInfo" :key="value.name">{{index}} {{key}} {{value}}</h3> -->
-    <!-- <template v-for="name in names" :key="name">
-      <h2 v-if="name === 'saber'">{{name}}<hr></h2>
-    </template> -->
-    <!-- <h2>Add method - {{add(2,3,5)}}</h2>
-    <h2>Add method - {{multiplay(10)}}</h2>
-    <h2>Add method - {{multiplay(arrValue)}}</h2> -->
-    <h1>{{name}}</h1>
+  <div>
+    <pre>
+    {{ JSON.stringify(formValues, null, 2) }}
+  </pre
+    >
+  </div>
+  <form @submit.prevent="submitForm">
     <div>
-      <button @click="changeName($event), increment(1,$event)">Change Name</button>  
-      <!-- multiple event handler -->
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model.trim.lazy="formValues.name" />
     </div>
-    <h2>{{count}}</h2>
-    <button @click="increment(1,$event)">increment</button>
-    <button @click="decrement(1)">decrement</button>
-    <button @click="increment(5)">increment 5</button>
+    <div>
+      <label for="profile">Profile</label>
+      <textarea
+        name="profile"
+        id="profile"
+        v-model="formValues.profileSummary"
+      ></textarea>
+    </div>
+    <div>
+      <label for="country">Country</label>
+      <select id="country" v-model="formValues.country">
+        <option value="">Select a country</option>
+        <option value="india">India</option>
+        <option value="Afghanistan">Afghanistan</option>
+        <option value="USA">USA</option>
+      </select>
+    </div>
+    <div>
+      <label for="job-location">Job Location</label>
+      <select id="job-location" multiple v-model="formValues.jobLocation">
+        <option value="india">India</option>
+        <option value="Afghanistan">Afghanistan</option>
+        <option value="USA">USA</option>
+      </select>
+    </div>
+    <div>
+      <input
+        type="checkbox"
+        id="remoteWork"
+        v-model="formValues.remoteWork"
+        true-value="yes"
+        false-value="no"
+      />
+      <label for="remoteWork">Open to remote Work?</label>
+    </div>
+    <div>
+      <label>Skill Set</label>
+      <input type="checkbox" id="html" value="html" v-model="formValues.skillSet">
+      <label for="html">HTML</label>
+      <input type="checkbox" id="css" value="css" v-model="formValues.skillSet">
+      <label for="css">CSS</label>
+      <input type="checkbox" id="js" value="js" v-model="formValues.skillSet">
+      <label for="js">JavaScript</label>
+    </div>
+      <div>
+      <label>Years of Experience</label>
+      <input type="radio" id="0-2" value="0-2" v-model="formValues.yearsOfExperience">
+      <label for="0-2">0-2</label>
+      <input type="radio" id="3-5" value="3-5" v-model="formValues.yearsOfExperience">
+      <label for="3-5">3-5</label>
+      <input type="radio" id="6-10" value="6-10" v-model="formValues.yearsOfExperience">
+      <label for="6-10">6-10</label>
+    </div>
+    <div>
+      <label for="age">Age</label>
+      <input type="number" @keyup.enter="submitForm" id="age" v-model.number="formValues.age">
+    </div>
+    <!-- <div>
+      <button>Submit</button>
+    </div> -->
+  </form>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
-    return{
-      basevalue: 5,
-      arrValue: 2,
-      names: ['saber','seerat','samim'],
-      fullname :[
-        {first: "saber",last: 'seerat'},
-        {first: "yaser",last: 'zahed'},
-        {first: "seyar",last: 'samim'},
-      ],
-      myInfo: {
-        name: 'saber',
-        channel: 'Lajaward',
-        course : 'VueJS'
+    return {
+      formValues: {
+        name: "",
+        profileSummary: "",
+        country: "",
+        jobLocation: [],
+        remoteWork: "no",
+        skillSet: [],
+        yearsOfExperience: '',
+        age: null,
       },
-      name: 'Saber',
-      count: 0
-      // num: 'd',
-      // display: true
-      // greeting: 'Hello',
-      // name: 'saber',
-      // lastname: 'seerat',
-      // channel: '<b>Lajaward</b>',
-      // headindId : 'heading',
-      // isDisabled: true,
-      // status: 'danger',
-      // isPromoted:  true,
-      // isSoldOut: true,
-      // highlightColor: 'orange',
-      // headerSize: 50,
-      // headerStyleObject:{
-      //   color: 'orange',
-      //   fontSize: '50px',
-      //   padding: '20px'
-      // },
-      // baseStyleObject:{
-      //   fontSize: '50px',
-      //   padding: '10px',
-      // },
-      // successStyleObject:{
-      //   color: 'green',
-      //   backgroundColor: 'lightgreen',
-      //   border: '1px solid green',
-      //   padding: '20px'
-      // },
-      // dangerStyleObject:{
-      //   color: 'darked',
-      //   backgroundColor: 'red',
-      //   border: '1px solid darked'
-      // }
+    };
+  },
+  methods: {
+    submitForm(){
+      // event.preventDefault()
+      console.log('Form Values',this.formValues);
+
     }
   },
-  methods:{
-    add(a,b,c){
-      return a+b+c
-    },
-    multiplay(num) {
-      return num * this.basevalue
-    },
-    changeName(event){
-      this.name = "Batman"
-      console.log('Event',event);
-    },
-    increment(num,event){
-       this.count += num
-       console.log('Event',event);
-    },
-    decrement(num){
-       this.count -= num
-    }
-  }
-}
+};
 </script>
 
 <style>
@@ -135,20 +105,32 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
-/* .underlined {
-  text-decoration: underline;
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
 }
-.promoted{
-  font-style: italic;
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
 }
-.new {
-  color: olivedrab;
+input[type="text"],
+textarea,
+select {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.428;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
-.sold-out {
-  color: red;
-} */
 </style>
